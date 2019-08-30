9ccaafb16b6e1bafb03673dd39dc44b985b2ce58
yunlingfly <508821881@qq.com>
2019-08-30 11:37:41 +0800

    [FIX]@TableId

--- src/main/java/cn/yunlingfly/springbootmybatisplus/domain/entity/TestEntity.java
+++ src/main/java/cn/yunlingfly/springbootmybatisplus/domain/entity/TestEntity.java
@@ -1,14 +1,13 @@
 package cn.yunlingfly.springbootmybatisplus.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import javax.persistence.Id;
 
 @Data
 @TableName("test")
 public class TestEntity {
-    @Id
+    @TableId
     private String id;
     private String name;
 }
